8276eef082ca6fec5db07216e3f90bb83239d2e6
Ryan <rogoclub@outlook.com>
2021-02-15 19:36:58 -0800

    initial commit 2.0

---
+++ ObjectManagement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EngineSharp
+{
+    public class Internal {
+        public static List<GameObject> activeObjects = new List<GameObject>();
+    }
+
+    public class SharpGame
+    {
+        
+    }
+
+    public class GameObject
+    {
+
+    }
+
+
+}
---
+++ TestClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EngineSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
